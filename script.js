612eac2aa25b4a7ac76d31e087194a9d86e9d0af
tamasmeidlinger <meidlinger.tamas1@gmail.com>
2025-09-16 07:16:06 +0200

    Change navbar style
    Resizing is still buggy.

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ let sideBar = document.querySelector(".links");
 
 menuIcon.addEventListener("click", () => {
   sideBar.classList.add("side-bar-active");
+
+  setTimeout(() => {
+    menuIcon.style.display = "none";
+  }, 400);
 });
 
 closeIcon.addEventListener("click", () => {
   sideBar.classList.remove("side-bar-active");
+
+  setTimeout(() => {
+    menuIcon.style.display = "block";
+  }, 300);
 });
+
+function checkWidth() {
+  if (window.innerWidth > 460) {
+    sideBar.classList.remove("side-bar-active");
+  }
+}
+
+checkWidth();
+window.addEventListener("resize", checkWidth);
